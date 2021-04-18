41898ff64a3af1714b9f5f6dfa1787e3dfefec07
darrenmckenna <Darrenmckenna23@gmail.com>
2021-04-18 16:07:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { CardList } from './components/card-list/card-list.component.jsx';
+import { SearchBox } from './components/search-box/search-box';
 
 function App() {
+  const [monsters, setMonsters] = useState([]);
+  const [searchField, setSearchField] = useState('');
+  const [filteredMonsters, setFilteredMonsters] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((data) => {
+        setMonsters(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    const filteredMonsterList = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    setFilteredMonsters(filteredMonsterList);
+  }, [searchField, monsters]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Monsters Rolodex</h1>
+      <SearchBox
+        handleChange={(e) => setSearchField(e.target.value)}
+        placeholder={'Search Monsters...'}
+      />
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 }
